10bb3dd319e5dfe3b50531a88d59a4e8683b34ee
NikhilS01 <NikhilS01@G09INXNP33123X1.g09.fujitsu.local>
2023-11-27 23:45:45 +0530

    Import WebCourses in Perosnal laptop eclipse

---
+++ WebCourses/src/main/java/com/example/CourseController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public String courses() {
+		System.out.println("Welcome to Courses");
+		return "courses.jsp";
+	}
+}
